63d3a1cdc1b7ad56681732e4b9007091b9353303
King-andu21 <apook@hkhk.edu.ee>
2025-11-10 11:12:11 +0200

    Update harjutus02.py

--- harjutus02.py
+++ harjutus02.py
@@ -1,3 +1,49 @@
 import turtle
+import time
+turtle.speed(10)
+turtle.penup()
+turtle.goto(-220,0) #blu
+
+turtle.pendown()
+
+turtle.pensize(5)
+turtle.color("blue")
 turtle.circle(50)
-turtle.done
+
+turtle.penup() #blu
+
+turtle.goto(-110,0) #blackie
+
+turtle.pendown()
+
+turtle.color("black")
+turtle.circle(50)
+
+turtle.penup() #blackie
+
+turtle.goto(0,0) # red
+
+turtle.pendown()
+
+turtle.color("red")
+turtle.circle(50) # red
+
+turtle.penup()
+
+turtle.goto(-165,-50) #yel
+
+turtle.pendown()
+
+turtle.color("yellow")
+turtle.circle(50)
+
+turtle.penup() #yel
+
+turtle.goto(-55,-50) #green
+
+turtle.pendown()
+
+turtle.color("green")
+turtle.circle(50)
+
+turtle.done()
