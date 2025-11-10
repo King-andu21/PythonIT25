0fe571644c3d331179cde412af133f0429638629
King-andu21 <apook@hkhk.edu.ee>
2025-11-10 10:53:03 +0200

    Update

--- harjutus01.py
+++ harjutus01.py
@@ -1,10 +1,28 @@
 import turtle
-
+import time
 turtle.penup()
-turtle.goto(-300, 300)
+turtle.goto(-300, 300) # kolmnurga algus
 turtle.pendown()
 
 turtle.goto(-200, 300)
-turtle.goto(-250, 500)
+turtle.goto(-250, 400)
+turtle.goto(-300, 300) # kolmnurga lõpp
 
+turtle.penup()
+time.sleep(0.1)
+turtle.goto(-300, -100) # A tähe algus
+turtle.pendown()
+turtle.goto(-250,100)
+turtle.goto(-200, -100)
+turtle.goto(-210, -100)
+turtle.goto(-240,10)
+turtle.goto(-260,10)
+turtle.goto(-290, -100)
+turtle.goto(-300, -100)
+turtle.penup()
+turtle.goto(-250,60)
+turtle.pendown()
+turtle.goto(-260,30)
+turtle.goto(-240,30)
+turtle.goto(-250,60) # A tähe lõpp
 turtle.done()
