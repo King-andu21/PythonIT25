addd1a3b1a914a6e83c43a3bfe8fafca34f3ad74
King-andu21 <apook@hkhk.edu.ee>
2025-11-10 11:47:04 +0200

    Create harjutus03.py

---
+++ harjutus03.py
@@ -0,0 +1,34 @@
+# Autor: Andre Pook
+
+#---o---
+
+# Harjutus 3.1 (Tervitus)
+
+#---o---
+nimi="Andre Pook"
+vanus=int(16)
+keskmine_hinne= float(5.7)
+print(nimi ,",", vanus ,"aastat vana ja keskmine hinne on", keskmine_hinne)
+input("vajuta enterit, et edasi minna!")
+#---o---
+
+# Harjutus 3.2 (Ostunimekiri)
+
+#---o---
+toode= "porgandeid"
+kogus=int(3)
+hind=float(5.35)
+
+kogus=str(kogus)
+hind=str(hind)
+print("Soovin "+ toode+" "+ kogus+", mille tüki hind on "+ hind)
+input("vajuta enterit, et edasi minna!")
+#---o---
+
+# Harjutus 3.3 (Reisiplaan)
+
+#---o---
+sihtkoht= "Soome"
+Paevade_arv= int(5)
+oobimise_hind= float(30.50)
+print(sihtkoht, "reis kestab",Paevade_arv,"päeva ja üks öö maksab", oobimise_hind, "eurot")
