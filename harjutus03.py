1d0cd8295165eb45833f9a087047e921c272232d
King-andu21 <apook@hkhk.edu.ee>
2025-11-18 13:14:10 +0200

    Suur püüton

--- harjutus03.py
+++ harjutus03.py
@@ -1,5 +1,5 @@
 # Autor: Andre Pook
-
+import turtle
 #---o---
 
 # Harjutus 3.1 (Tervitus)
@@ -9,7 +9,6 @@ nimi="Andre Pook"
 vanus=int(16)
 keskmine_hinne= float(5.7)
 print(nimi ,",", vanus ,"aastat vana ja keskmine hinne on", keskmine_hinne)
-input("vajuta enterit, et edasi minna!")
 #---o---
 
 # Harjutus 3.2 (Ostunimekiri)
@@ -19,10 +18,7 @@ toode= "porgandeid"
 kogus=int(3)
 hind=float(5.35)
 
-kogus=str(kogus)
-hind=str(hind)
-print("Soovin "+ toode+" "+ kogus+", mille tüki hind on "+ hind)
-input("vajuta enterit, et edasi minna!")
+print("Soovin "+ toode+" "+ str(kogus)+", mille tüki hind on "+ str(hind))
 #---o---
 
 # Harjutus 3.3 (Reisiplaan)
@@ -32,3 +28,70 @@ sihtkoht= "Soome"
 Paevade_arv= int(5)
 oobimise_hind= float(30.50)
 print(sihtkoht, "reis kestab",Paevade_arv,"päeva ja üks öö maksab", oobimise_hind, "eurot")
+#---o---
+
+# Harjutus 3.4 (Lemmikraamat)
+
+#---o---
+raamatu_pealkiri = "Veebibeebi"
+raamatu_autor = "Imre Tard"
+raamat = raamatu_pealkiri + " " + raamatu_autor
+lehekylgede_arv = 50
+hindamisskoor = 8.8
+print("Minu lemmikraamat on"+raamat+", mis on"+ str(lehekylgede_arv) +"pikk ja mille ma hindad" +str(hindamisskoor)+ "punktiga")
+#---o---
+
+# Harjutus 3.5 (Unistuste auto)
+
+#---o---
+auto_mark = "Opel"
+auto_mudel = "Corsa"
+auto = auto_mark + " " + auto_mudel
+tootmisaasta = 2011
+hind = 9999.99
+print("minu unistuste auto on",auto,"aastast",tootmisaasta,", mille hind on ", hind,"eurot")
+#---o---
+
+# Harjutus 3.6 (Python Turtle ruut)
+
+#---o---
+kylje_pikkus = 50
+nurk = 90
+kujund_värv = "red"
+drawing = 0
+kordaja = 1.5
+turtle.color(kujund_värv)
+for drawing in range(3):
+    turtle.pendown()
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.penup()
+    turtle.forward(kylje_pikkus*kordaja)
+#---o---
+
+# Harjutus 3.7 (Python Turtle kolmnurk)
+
+#---o---
+kylje_pikkus = 50
+nurk = 120
+kujund_värv = "blue"
+drawing = 0
+kordaja = 1.5
+turtle.color(kujund_värv)
+for drawing in range(3):
+    turtle.pendown()
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.forward(kylje_pikkus)
+    turtle.left(nurk)
+    turtle.penup()
+    turtle.forward(kylje_pikkus*kordaja)
+turtle.done()
