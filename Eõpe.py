ac1ac66e997e6ce25b3eaa125583c72e981d5d13
King-andu21 <apook@hkhk.edu.ee>
2026-01-05 15:05:34 +0200

    Update Eõpe.py

--- Eõpe.py
+++ Eõpe.py
@@ -1,2 +1,39 @@
+# Autor: Andre Pook
+# Kuupäev: 05.01.2026
 import turtle
-print("test")
+
+
+# ---
+# Esimene Muster
+# ---
+turtle.speed(0)
+mustri_suurus = 100
+for i in range(4):
+    for i in range(3):
+        turtle.forward(mustri_suurus)
+        turtle.left(90)
+        turtle.forward(mustri_suurus)
+        turtle.right(90)
+        turtle.forward(mustri_suurus)
+        turtle.right(90)
+    turtle.forward(mustri_suurus)
+    turtle.left(90)
+    turtle.forward(mustri_suurus)
+    turtle.right(90)
+    turtle.forward(mustri_suurus)
+
+# ---
+# Teine Muster
+# ---
+mustri_suurus = 120
+for i in range(5):
+    turtle.forward(mustri_suurus/2)
+    turtle.left(120)
+    turtle.forward(mustri_suurus)
+    turtle.left(120)
+    turtle.forward(mustri_suurus)
+    turtle.left(120)
+    turtle.forward(mustri_suurus/2)
+    turtle.right(72)
+
+turtle.done()
