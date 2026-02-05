5f50b102607fcf35ec0540e0975741f5c9a4beec
King-andu21 <apook@hkhk.edu.ee>
2026-02-05 11:28:47 +0200

    more fixes to file locations

--- Iseseisev_too_nr3/3.4.py
+++ Iseseisev_too_nr3/3.4.py
@@ -1,7 +1,7 @@
 import os
-print(os.listdir('.'))
+print(os.listdir('PythonIT25/files/muusika'))
 chosen_file = input("Sisesta failinimi: ")
-f = open(f"PythonIT25/files/{chosen_file}", encoding="UTF-8")
+f = open(f"PythonIT25/files/muusika/{chosen_file}", encoding="UTF-8")
 jark = 0
 for rida in f:
     print(f"{jark}. {rida}")
@@ -9,7 +9,7 @@ for rida in f:
 chosen_song = int(input("Valige laulu järjekorranumber: "))
 
 jark = 0
-f = open(f"PythonIT25/files/{chosen_file}", encoding="UTF-8")
+f = open(f"PythonIT25/files/muusika/{chosen_file}", encoding="UTF-8")
 for rida in f:
     if chosen_song == jark:
         print(f"{jark}. {rida}")
