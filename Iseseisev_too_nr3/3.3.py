d4f3eef25b569fc1d51db8e90ee638df20a946b3
King-andu21 <apook@hkhk.edu.ee>
2026-02-05 11:21:47 +0200

    e

--- Iseseisev_too_nr3/3.3.py
+++ Iseseisev_too_nr3/3.3.py
@@ -1,4 +1,4 @@
-fail = open("Python_IT25_Uus/files/konto.txt", encoding="UTF-8")
+fail = open("PythonIT25/files/konto.txt", encoding="UTF-8")
 for rida in fail:
     if float(rida) > 0:
         print(float(rida))
