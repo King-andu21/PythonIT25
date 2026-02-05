6e43c6230494a465d66b2ad2c7bc743f0913fdc5
King-andu21 <apook@hkhk.edu.ee>
2026-02-05 11:41:30 +0200

    better spelling

--- Iseseisev_too_nr4/4.2.py
+++ Iseseisev_too_nr4/4.2.py
@@ -2,5 +2,5 @@ def mahlapakkide_arv(ountekogus):
     mahlapakkide_arv = ountekogus * 0.4/3
     mahlapakkide_arv = round(mahlapakkide_arv)
     return mahlapakkide_arv
-ountekogus = float(input("sisesta õunte arv: "))
+ountekogus = float(input("sisesta õunte arv kilogrammides: "))
 print(mahlapakkide_arv(ountekogus))
